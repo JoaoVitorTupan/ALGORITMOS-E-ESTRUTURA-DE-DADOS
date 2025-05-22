10879a507fc89986a439d6b59cf299f0c493b7f1
João Vitor Tupan Dias <joaovtupan@gmail.com>
2025-05-22 16:09:12 -0300

    segundo commit

--- Arvore.java
+++ Arvore.java
@@ -1,3 +1,34 @@
 public class Arvore { 
-    No raiz; 
+    No raiz;
+
+    public Arvore() {
+        this.raiz = null;
+    }
+
+    public void inserir(int valor) {
+        No novo = new No(valor);
+
+        if (raiz == null) {
+            raiz = novo;
+            return;
+        }
+
+        No atual = raiz;
+
+        while (true) {
+            if (valor < atual.valor) {
+                if (atual.esquerda == null) {
+                    atual.esquerda = novo;
+                    return;
+                }
+                atual = atual.esquerda;
+            } else {
+                if (atual.direita == null) {
+                    atual.direita = novo;
+                    return;
+                }
+                atual = atual.direita;
+            }
+        }
+    }
 }
---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        Arvore minhaArvore = new Arvore();
+
+        minhaArvore.inserir(10);
+        minhaArvore.inserir(5);
+        minhaArvore.inserir(15);
+        minhaArvore.inserir(3);
+    }
+}
