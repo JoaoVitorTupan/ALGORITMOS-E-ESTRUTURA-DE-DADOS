1c9e74a36ff6ed5dc415eacb80f440801ce038b7
JoaoVitorTupan <168040620+JoaoVitorTupan@users.noreply.github.com>
2025-05-29 16:11:58 -0300

    sexto commit

--- Arvore.java
+++ Arvore.java
@@ -31,6 +31,11 @@ public class Arvore {
             }
         }
     }
+    
+    public int contarNos (No node) { 
+    if (node == null) return 0;
+    return 1 + contarNos(node.esquerda) + contarNos(node.direita);
+    }
 
     public void preOrdem (No node) { 
         if (node != null) {
@@ -55,4 +60,5 @@ public class Arvore {
             System.out.print(node.valor + " ");
         }
     }
+
 }
